8c5d38a9658eef658975e30a7e0669097e959e91
Daniel Nicolae <cdn27@cam.ac.uk>
2023-12-28 12:07:44 +0200

    computed variation modes

--- src/init.py
+++ src/init.py
@@ -1,7 +1,7 @@
 from landmarks import copy_landmarks
 from segmentations import compute_segments_dict
 from fit import compute_fiducials_dicts, correct_fiducials_dicts, left, right, canals
-
+from variations import compute_variation_modes
 
 for i in range(1, 54):
     copy_landmarks(i)
@@ -9,3 +9,6 @@ for i in range(1, 54):
 compute_segments_dict(save_pickle=True)
 compute_fiducials_dicts(save_pickle=True)
 correct_fiducials_dicts(save_pickle=True)
+
+for canal in canals:
+    compute_variation_modes(right, canal, save_pickle=True)
