8a59f9e8956bd78dffad65aeca5121901954f47e
Daniel Nicolae <cdn27@cam.ac.uk>
2024-02-05 21:36:53 +0000

    model able to detect landmarks

--- epley_web/src/components/model.tsx
+++ epley_web/src/components/model.tsx
@@ -2,13 +2,13 @@ import "@tensorflow/tfjs-core";
 import "@tensorflow/tfjs-backend-webgl"
 import '@mediapipe/face_mesh';
 import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
-import { MediaPipeFaceMeshTfjsModelConfig, FaceLandmarksDetector } from "@tensorflow-models/face-landmarks-detection";
-
+import { MediaPipeFaceMeshMediaPipeModelConfig, FaceLandmarksDetector } from "@tensorflow-models/face-landmarks-detection";
 
 export const runDetector = async (video: HTMLVideoElement) => {
     const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
-    const detectorConfig: MediaPipeFaceMeshTfjsModelConfig = {
-        runtime: "tfjs",
+    const detectorConfig: MediaPipeFaceMeshMediaPipeModelConfig = {
+        runtime: "mediapipe",
+        solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
         refineLandmarks: false
     };
     const detector = await faceLandmarksDetection.createDetector(
@@ -19,7 +19,7 @@ export const runDetector = async (video: HTMLVideoElement) => {
         const estimationConfig = { flipHorizontal: false };
         const faces = await detector.estimateFaces(video, estimationConfig);
         console.log(faces)
-        detect(detector)
+        // detect(detector)
     };
     detect(detector);
   };
