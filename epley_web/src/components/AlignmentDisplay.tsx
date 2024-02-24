d94185dba96d4f03c94858d885bf00f503e29c2c
Daniel Nicolae <cdn27@cam.ac.uk>
2024-02-24 20:21:57 +0000

    added live countdown

--- epley_web/src/components/AlignmentDisplay.tsx
+++ epley_web/src/components/AlignmentDisplay.tsx
@@ -12,10 +12,13 @@ interface Props {
 
 const GREEN = "#11bb22"
 const BLACK = "#000000"
+const THRESHOLD = 0.3
+const DURATION = 5.0
 
 const AlignmentDisplay = ({stage, canal, stageCallback, alignmentCallback, cameraCallback}: Props) => {
     const [alignment, setAligment] = useState(0)
     const [color, setColor] = useState(BLACK)
+    const [displayTimer, setDisplayTimer] = useState(0.0)
     const loop = useRef<NodeJS.Timer>()
 
     const [playAligned] = useSound("sounds/aligned.mp3")
@@ -29,25 +32,35 @@ const AlignmentDisplay = ({stage, canal, stageCallback, alignmentCallback, camer
             loop.current = setInterval(() => {
                 const newAlignment = alignmentCallback()
                 setAligment(newAlignment)
-                if (newAlignment > 0.7) {
+                if (newAlignment > THRESHOLD) {
                     if (timer === 0.0) {
                         setColor(GREEN)
                         playAligned()
                     }
                     timer += 0.15
-                    if (timer > 5.0) {
+                    setDisplayTimer(timer)
+                    if (timer > DURATION) {
                         stageCallback((stage + 1) % meshPartsLength[canal]) 
                         if (stage === 1) cameraCallback()
+                        timer = 0.0
+                        setDisplayTimer(0.0)
                     }
                 } else if (timer !== 0.0) {
                         timer = 0.0
+                        setDisplayTimer(0.0)
                         setColor(BLACK)
                         playNotAligned()
                     }
             }, 150)
     }, [stage])
 
-    return <h4 style={{color: color}}>Alignment: {(alignment*100).toFixed(2)}%</h4>
+    return (
+        <div style={{display: "flex", width: "100%", flexDirection: "row", justifyContent: "space-around"}}>
+            <h4 style={{color: color}}>Alignment: {(alignment*100).toFixed(2)}%</h4>
+            {displayTimer !== 0.0 && <h4>Keep for {(DURATION - displayTimer).toFixed(2)} seconds.</h4>}
+        </div>
+    ) 
+    
 }
 
 export default AlignmentDisplay
